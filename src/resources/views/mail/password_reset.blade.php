223e6fc7c770a1891bdba2128e86a63a8a33c88b
Nurdaulet <nurik9293709@gmail.com>
2019-12-18 14:06:29 +0600

    route not found fix

--- src/resources/views/mail/password_reset.blade.php
+++ src/resources/views/mail/password_reset.blade.php
@@ -1,1 +1,1 @@
-<a href="{{ route('verify', ['login' => $token->login, 'token' => $token->token]) }}">new password is {{ $password }}</a>
+<a href="{{ route('bs-auth-verify', ['login' => $token->login, 'token' => $token->token]) }}">new password is {{ $password }}</a>
