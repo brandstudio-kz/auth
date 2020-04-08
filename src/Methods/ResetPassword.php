06e2028b31523bfcf841df86e575dce78bf52f48
Nurdaulet <nurik9293709@gmail.com>
2020-04-08 19:18:03 +0600

    reset password fix

--- src/Methods/ResetPassword.php
+++ src/Methods/ResetPassword.php
@@ -3,6 +3,8 @@
 namespace BrandStudio\Auth\Methods;
 
 use BrandStudio\Auth\AuthService;
+use BrandStudio\Auth\Jobs\SendSmsJob;
+use Illuminate\Support\Facades\Hash;
 
 class ResetPassword extends Base
 {
@@ -10,7 +12,17 @@ class ResetPassword extends Base
     public static function execute(AuthService $authService, string $login)
     {
         $user = $authService->getUser($login);
-        $authService->createVerificationToken($user->id, $login, $authService->generatePassword());
+        if ($authService->getAuthFieldType($login) == 'phone') {
+            $password = $authService->generatePassword();
+
+            $user->update([
+                'password' => Hash::make($password),
+            ]);
+            SendSmsJob::dispatch($login, trans('brandstudio::auth.your_password', ['password' => $password]));
+
+        } else {
+            $authService->createVerificationToken($user->id, $login, $authService->generatePassword());
+        }
     }
 
 }
